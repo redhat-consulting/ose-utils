9fc525de5190650b5fa94718218b875eb6ad71e5
John Kwiatkoski <jkwiatko@redhat.com>
2015-02-02 10:34:54 -0800

    oo-register-application.sh fixed output and added appDomainFullyQualified to JSON

--- oo-register-application.sh
+++ oo-register-application.sh
@@ -2,12 +2,16 @@
 #oo-register-application.sh {gearProfile}{appDomain} [--appAdmins {appAdminsCSV}] [--developers {developersCSV}]
 #SETUP
 # Set the broker host variable below to the correct url for the broker and [OPTIONAL] set the logfile where to send stdout & stderr
-# Set the internalUser to the user who will own authenticate to register the application
+# Set the internalUser to the user who will own authenticate to register the application "appDomain" in the Onboard flows document
 #
 #PARAMETERS
 #
-#gearProfile: gear profile to be used in the application eg. small, medium
-#appDomain: the domain of the application
+#[REQUIRED]gearProfile: gear profile to be used in the application eg. small, medium
+#[REQUIRED]appDomain: the domain of the application eg. applicationX
+#[OPTIONAL]--appAdmins <comma-separtated list> of users to be granted admin access
+#[OPTIONAL]--developers <comma-separtated list> of users to be granted developer access
+#IMPORTANT NOTE: Comma-separated lists may not contain spaces.
+#
 #
 #VARIABLES
 #logFile: where the stderr and stdout get redirected to
@@ -15,8 +19,8 @@
 #
 #
 #EXAMPLES
-# ./oo-register-developer.sh Jsmith small
-# ./oo-register-developer.sh Bnye medium
+# ./oo-register-application.sh small app1 --appAdmins kyle1 --developers ryanT,lucy34
+# ./oo-register-application.sh small applicationx --appAdmins kyle1,rebecca4
 #
 #
 
@@ -36,25 +40,20 @@ function json {
         'returnCode':'$1',
         'returnDesc':'$2',
         'authenticationTokenId':'$3'
-
+        'appDomainFullyQualified':'$4'
   }"
 
 }
 
 function exit_code {
-  IFS=',' read -ra json <<< $1
+  IFS=',' read -ra json <<< "$1"
   for section in ${json[*]}; do
       if [[ $section == *"exit_code"* ]]; then
-      code=$(grep -o "[0-9]" <<< "$section")
-      code=$(sed 's/ //g' <<< $code)
-      echo "$code"
-      export status=$code
-
+        code=$(grep -o "[0-9]" <<< "$section")
+        code=$(sed 's/ //g' <<< $code)
+        echo "$code"
       fi
   done
-  if [ $status = -1 ];then
-     export status=0
-  fi
 }
 
 if [ "$#" -le 1 ]
@@ -66,10 +65,11 @@ fi
 
 gearProfile="$1"
 appDomain="$2"
+
 if [ -z ${gearProfile+x} ] || [ -z ${appDomain+x} ]
   then
   usage
-  json 1 "gearProfile or appDomain not set" ""
+  json 255 "gearProfile or appDomain not set" ""
   exit 1
 fi
 if [ -z ${3+x} ]
@@ -97,36 +97,36 @@ elif [ "$5" = "--developers" ]
 fi
 
 #1. Create internal Openshift user "$internalUser"
-oo-admin-ctl-user -c -l $internalUser
+oo-admin-ctl-user -c -l $internalUser &>>$logfile
 if [ "$?" = "0" ]; then
-  echo "The user $internalUser already exists!"
+  echo "The user $internalUser already exists!" &>>$logfile
 
 fi
 
 
 #Create Token (expires a year from creation
-TOKEN="$(oo-auth-token -l $internalUser -e "$(date -d "+year")")"
+TOKEN="$(oo-auth-token -l $internalUser -e "$(date -d "+year" 2>> $logfile | tee $logFile)" 2>> $logfile | tee -a $logFile)"
 
 
 #2. Create application domain owned by internal Openshift user $internalUser
-response=$( curl -k -H "Authorization: Bearer $TOKEN" -X POST https://$brokerhost/broker/rest/domains/ --data-urlencode name=$appDomain --data-urlencode allowed_gear_sizes=small)
-exit_code $response
-
-if [ $status != 0 ];then
-    json 1 "Openshift API exit code $status"
+response=$( curl -k -H "Authorization: Bearer $TOKEN" -X POST https://$brokerhost/broker/rest/domains/ --data-urlencode name=$appDomain --data-urlencode allowed_gear_sizes=small 2>> $logfile | tee -a $logFile 2>> $logfile)
+status=$(exit_code "$response")
+if [ "$status" = 103 ];then
+    json 1 "Domain already exists. Openshift API exit code $status"
     exit 1
+elif [ "$status" != 0 ];then
+     json 255 "Error creating Domain. Openshift API exit code $status"
+     exit 255
 fi
 
 
 #4. grant edit access on the application domain to all employeeNums listed under appAdmins
 IFS=',' read -ra admins <<< $appAdminCVS
-echo ""
 for employee in ${admins[*]}; do
-    response=$(curl -k -H "Authorization: Bearer $TOKEN" -X PATCH https://$brokerhost/broker/rest/domains/$appDomain/members --data-urlencode role=admin --data-urlencode login=$employee)
-    exit_code $response
-    if [ $status != 0 ];then
-        json 255 "Openshift API exit code $status"
-        echo "2"
+    response=$(curl -k -H "Authorization: Bearer $TOKEN" -X PATCH https://$brokerhost/broker/rest/domains/$appDomain/members --data-urlencode role=admin --data-urlencode login=$employee 2>> $logfile | tee -a $logFile )
+    status=$(exit_code "$response")
+      if [ "$status" != 0 ];then
+        json 255 "Error granting administrator permissions. Openshift API exit code $status"
         exit 255
     fi
 done
@@ -135,15 +135,15 @@ done
 
 IFS=',' read -ra devs <<< $developers
 
-
 for dev in ${devs[*]}; do
-    response=$(curl -k -H "Authorization: Bearer $TOKEN" -X PATCH https://$brokerhost/broker/rest/domains/$appDomain/members --data-urlencode role=view --data-urlencode login=$dev)
-    exit_code $response
+    response=$(curl -k -H "Authorization: Bearer $TOKEN" -X PATCH https://$brokerhost/broker/rest/domains/$appDomain/members --data-urlencode role=view --data-urlencode login=$dev 2>> $logfile| tee -a $logFile )
+    status=$(exit_code "$response")
     if [ $status != 0 ];then
-        json 255 "Openshift API exit code $status"
+        json 255 "Error granting developer permissions. Openshift API exit code $status"
+        echo "3"
         exit 255
     fi
 done
 
-json 0 "Success" $TOKEN
+json 0 "Success" $TOKEN "$appDomain.$brokerhost"
 exit 0
